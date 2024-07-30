55dd7b72a9ab0f8c8ffacaaa7e528a4f8c348a1a
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-07-30 23:48:04 +0700

    hoan thanh admin add product cho du an

--- frontend-Ecommerce/dashboard/src/components/admin/ProductDashboard.jsx
+++ frontend-Ecommerce/dashboard/src/components/admin/ProductDashboard.jsx
@@ -4,6 +4,7 @@ import Pagination from "../Pagination";
 import { FaEdit } from "react-icons/fa";
 import { BiSolidTrashAlt } from "react-icons/bi";
 import { FaImage } from "react-icons/fa6";
+import { IoCloseCircle } from "react-icons/io5";
 
 const ProductDashboard = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -13,6 +14,16 @@ const ProductDashboard = () => {
 
   return (
     <div className="px-2 pt-5 lg:px-7">
+      <div className="flex items-center justify-between p-4 mb-5 bg-white rounded-md lg:hidden">
+        <h1 className="text-lg font-semibold">Product</h1>
+        <button
+          onClick={() => setShow(true)}
+          className="bg-[#fc334d] shadow-lg  hover:shadow-red-500/50 px-4 py-2 cursor-pointer rounded-sm font-semibold"
+        >
+          Add
+        </button>
+      </div>
+
       <div className="flex flex-wrap w-full">
         <div className="w-full lg:w-7/12 ">
           <div className="w-full p-4 bg-white rounded-md">
@@ -58,19 +69,19 @@ const ProductDashboard = () => {
                     <tr key={index}>
                       <td
                         scope="row"
-                        className="px-4 py-2 font-medium whitespace-nowrap"
+                        className="px-4 py-[6.7px] font-medium whitespace-nowrap"
                       >
                         {k}
                       </td>
                       <td
                         scope="row"
-                        className="px-4 py-2 font-medium whitespace-nowrap"
+                        className="px-4 py-[6.7px] font-medium whitespace-nowrap"
                       >
                         Clothes
                       </td>
                       <td
                         scope="row"
-                        className="flex items-center justify-center px-4 py-2 font-medium whitespace-nowrap"
+                        className="flex items-center justify-center px-4 py-[6.7px] font-medium whitespace-nowrap"
                       >
                         <img
                           className="w-[45px] h-[45px]"
@@ -81,7 +92,7 @@ const ProductDashboard = () => {
 
                       <td
                         scope="row"
-                        className="px-4 py-2 font-medium whitespace-nowrap "
+                        className="px-4 py-[6.7px] font-medium whitespace-nowrap "
                       >
                         <div className="flex items-center justify-center gap-4">
                           <Link className="p-[6px] bg-[#51a8ff] rounded hover:shadow-lg hover:shadow-blue-500/50 text-white">
@@ -98,7 +109,7 @@ const ProductDashboard = () => {
               </table>
             </div>
 
-            <div className="flex justify-end w-full pr-6 mt-4 bottom-4 right-4">
+            <div className="flex justify-end w-full mt-4 bottom-4 right-4">
               <Pagination
                 pageNumber={currentPage}
                 setPageNumber={setCurrentPage}
@@ -112,13 +123,20 @@ const ProductDashboard = () => {
         <div
           className={`w-[320px] lg:w-5/12 translate-x-100 lg:relative lg:right-0 fixed ${
             show ? "right-0" : "-right-[340px]"
-          } z-20 top-0 transition-none duration-500`}
+          } z-[100] top-0 transition-all duration-500`}
         >
           <div className="w-full pl-5 ">
             <div className="h-screen px-3 py-2 bg-white lg:h-auto lg:rounded-md">
-              <h1 className="w-full mb-4 text-xl font-semibold text-center">
-                Add Product
-              </h1>
+              <div className="flex items-center justify-between mb-4 ">
+                <h1 className="w-full mb-4 text-xl font-semibold text-center">
+                  Add Product
+                </h1>
+
+                <div onClick={() => setShow(false)} className="block lg:hidden">
+                  <IoCloseCircle />
+                </div>
+              </div>
+
               <form action="">
                 <div className="flex flex-col w-full gap-1 mb-3">
                   <label htmlFor="name">Product Name</label>
@@ -148,7 +166,7 @@ const ProductDashboard = () => {
                     id="image"
                   />
                   <div>
-                    <button className="w-full bg-[#fc334d]  hover:shadow-red-500/50 hover:shadow-sm rounded-md px-7 py-2 my-2">
+                    <button className="w-full bg-[#fc334d]  hover:shadow-red-500/50 hover:shadow-sm rounded-md px-7 py-3 my-2">
                       Add Product
                     </button>
                   </div>
