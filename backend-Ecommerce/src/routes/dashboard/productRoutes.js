16c53425c94b17a4a0f6e19792d1f9e821568d25
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-09-15 11:41:21 +0700

    them product routes cho du an

--- backend-Ecommerce/src/routes/dashboard/productRoutes.js
+++ backend-Ecommerce/src/routes/dashboard/productRoutes.js
@@ -4,5 +4,9 @@ const { authMiddleware } = require("../../middlewares/authMiddleware");
 const router = require("express").Router();
 
 router.post("/product-add", authMiddleware, productController.add_product);
+router.get("/products-get", authMiddleware, productController.get_products);
+router.get("/product-get/:productId", authMiddleware, productController.get_product);
+router.post("/product-update", authMiddleware, productController.update_product);
+router.post("/product-image-update", authMiddleware, productController.update_product_image);
 
 module.exports = router;
