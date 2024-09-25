ffa788858e4f1dfeebb8fa2a9281e772ccb8cfeb
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-09-25 22:28:40 +0700

    giai quyet van de security

--- frontend-Ecommerce/dashboard/vite.config.js
+++ frontend-Ecommerce/dashboard/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    eslint({
-      include: ["src/**/*.js", "src/**/*.jsx", "src/**/*.ts", "src/**/*.tsx"],
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
     // watch: {
