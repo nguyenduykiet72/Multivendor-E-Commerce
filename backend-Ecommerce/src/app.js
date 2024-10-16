f0f5617924a347380598795ee8132632ffb11773
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-10-16 17:18:23 +0700

    them cau hinh va route

--- backend-Ecommerce/src/app.js
+++ backend-Ecommerce/src/app.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
 const morgan = require("morgan");
 const { dbConnect } = require("./utils/db");
 require("dotenv").config();
 
-
 const authRoutes = require("./routes/authRoutes");
 const categoryRoutes = require("./routes/dashboard/categoryRoutes");
 const productRoutes = require("./routes/dashboard/productRoutes");
 const sellerRoutes = require("./routes/dashboard/sellerRoutes");
-const homeRoutes = require('./routes/home/homeRoutes')
+const homeRoutes = require("./routes/home/homeRoutes");
+const customerRoutes = require("./routes/home/customerAuthRoutes");
+const cartRoutes = require("./routes/home/cartRoutes");
+const orderRoutes = require("./routes/order/orderRoute");
+const chatRoutes = require("./routes/chatRoutes");
 
 app.use(morgan("dev"));
 app.use(compression());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     credentials: true,
     allowedHeaders: [
       "Origin",
@@ -31,13 +33,18 @@ app.use(
   })
 );
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api", authRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", sellerRoutes);
 app.use("/api", homeRoutes);
+app.use("/api", customerRoutes);
+app.use("/api", cartRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", chatRoutes);
 dbConnect();
 
 module.exports = app;
