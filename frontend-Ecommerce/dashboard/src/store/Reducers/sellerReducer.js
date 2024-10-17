e5f38a53c49dd2df3889702b0d6993f985b54713
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-10-18 00:09:46 +0700

    them function trong seller reducer

--- frontend-Ecommerce/dashboard/src/store/Reducers/sellerReducer.js
+++ frontend-Ecommerce/dashboard/src/store/Reducers/sellerReducer.js
@@ -74,6 +74,26 @@ export const get_active_sellers = createAsyncThunk(
   }
 );
 
+export const get_deactivate_sellers = createAsyncThunk(
+  "seller/get_deactivate_sellers",
+  async (
+    { nextPage, page, searchValue },
+    { rejectWithValue, fulfillWithValue }
+  ) => {
+    try {
+      const { data } = await api.get(
+        `/get-deactivate-seller?page=${page}&&searchValue=${searchValue}&&nextPage=${nextPage}`,
+        {
+          withCredentials: true,
+        }
+      );
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
 export const sellerReducer = createSlice({
   name: "seller",
   initialState: {
@@ -103,7 +123,16 @@ export const sellerReducer = createSlice({
       .addCase(seller_update_status.fulfilled, (state, { payload }) => {
         state.seller = payload.seller;
         state.successMessage = payload.message;
-      });
+      })
+
+      .addCase(get_active_sellers.fulfilled, (state, { payload }) => {
+        state.sellers = payload.sellers;
+        state.totalSeller = payload.totalSeller;
+      })
+      .addCase(get_deactivate_sellers.fulfilled, (state, { payload }) => {
+        state.sellers = payload.sellers;
+        state.totalSeller = payload.totalSeller;
+      })
   },
 });
 
