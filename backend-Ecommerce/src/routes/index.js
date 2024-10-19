e35d75adbc3cfb1ac9f6dc3cda6a072d3b820777
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-10-19 23:30:00 +0700

    chuyen routes vao file index va tao payment route

---
+++ backend-Ecommerce/src/routes/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const authRoutes = require("./authRoutes");
+const categoryRoutes = require("./dashboard/categoryRoutes");
+const productRoutes = require("./dashboard/productRoutes");
+const sellerRoutes = require("./dashboard/sellerRoutes");
+const homeRoutes = require("./home/homeRoutes");
+const customerRoutes = require("./home/customerAuthRoutes");
+const cartRoutes = require("./home/cartRoutes");
+const orderRoutes = require("./order/orderRoute");
+const chatRoutes = require("./chatRoutes");
+const paymentRoutes = require("./payment/paymentRoutes");
+
+router.use("/api", authRoutes);
+router.use("/api", categoryRoutes);
+router.use("/api", productRoutes);
+router.use("/api", sellerRoutes);
+router.use("/api", homeRoutes);
+router.use("/api", customerRoutes);
+router.use("/api", cartRoutes);
+router.use("/api", orderRoutes);
+router.use("/api", chatRoutes);
+router.use("/api", paymentRoutes);
+
+module.exports = router;
---
+++ backend-Ecommerce/src/routes/payment/paymentRoutes.js
@@ -0,0 +1,13 @@
+const paymentController = require("../../controllers/payment/paymentControllers");
+const { authMiddleware } = require("../../middlewares/authMiddleware");
+const router = require("express").Router();
+
+router.get(
+  "/payment/create-stripe-connect-account",
+  authMiddleware,
+  paymentController.create_stripe_connect_account
+);
+
+
+
+module.exports = router;
