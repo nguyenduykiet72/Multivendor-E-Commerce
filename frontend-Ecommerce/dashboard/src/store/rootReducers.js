53702af5590eced956a5cc66adc710a5867f9bf0
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-10-25 21:39:08 +0700

    them payment trong rootReducer

--- frontend-Ecommerce/dashboard/src/store/rootReducers.js
+++ frontend-Ecommerce/dashboard/src/store/rootReducers.js
@@ -2,6 +2,7 @@ import authReducer from "./Reducers/authReducer";
 import categoryReducer from "./Reducers/categoryReducer";
 import chatReducer from "./Reducers/chatReducer";
 import orderReducer from "./Reducers/oderReducer";
+import paymentReducer from "./Reducers/paymentReducer";
 import productReducer from "./Reducers/productReducer";
 import sellerReducer from "./Reducers/sellerReducer";
 
@@ -12,6 +13,7 @@ const rootReducer = {
   seller: sellerReducer,
   chat: chatReducer,
   order: orderReducer,
+  payment: paymentReducer,
 };
 
 export default rootReducer;
