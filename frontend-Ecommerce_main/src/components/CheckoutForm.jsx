9a5909e8d0d9dd97ccf7d7e7ba0d7fa58a208e00
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-10-25 16:39:19 +0700

    them confirmOrder page va checkoutForm component

---
+++ frontend-Ecommerce_main/src/components/CheckoutForm.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import {
+  PaymentElement,
+  LinkAuthenticationElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+const CheckoutForm = ({ orderId }) => {
+  localStorage.setItem("orderId", orderId);
+  const stripe = useStripe();
+  const elements = useElements();
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const paymentElementOptions = {
+    layout: "tabs",
+  };
+
+  const submit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    setIsLoading(true);
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: "http://localhost:3000/order/confirm",
+      },
+    });
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
+    } else {
+      setMessage("An unexpected error occured. Please try again.");
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <form onSubmit={submit} id="payment-form">
+      <LinkAuthenticationElement id="link-authentication-element" />
+      <PaymentElement id="payment-element" options={paymentElementOptions} />
+      <button
+        disabled={isLoading || !stripe || !elements}
+        id="submit"
+        className="px-10 py-[6px] rounded-sm hover:shadow-green-700/30 hover:shadow-md bg-green-600 text-white"
+      >
+        <span id="button-text">
+          {isLoading ? <div>Loading...</div> : "Pay Now"}
+        </span>
+      </button>
+      {message && <div>{message}</div>}
+    </form>
+  );
+};
+
+export default CheckoutForm;
---
+++ frontend-Ecommerce_main/src/pages/ConfirmOrder.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Link } from "react-router-dom";
+import { FadeLoader } from "react-spinners";
+import axios from "axios";
+
+const load = async () => {
+  return await loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+};
+
+const ConfirmOrder = () => {
+  const [loader, setLoader] = useState(true);
+  const [stripe, setStripe] = useState("");
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+    if (!clientSecret) {
+      return;
+    }
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("succeeded");
+          break;
+
+        case "processing":
+          setMessage("processing");
+          break;
+        case "requires_payment_method":
+          setMessage("failed");
+          break;
+
+        default:
+          setMessage("failed");
+          break;
+      }
+    });
+  }, [stripe]);
+
+  const get_load = async () => {
+    const tempStripe = await load();
+    setStripe(tempStripe);
+  };
+
+  useEffect(() => {
+    get_load();
+  }, []);
+
+  const update_payment = async () => {
+    const orderId = localStorage.getItem("orderId");
+    if (orderId) {
+      try {
+        await axios.get(`http://localhost:8080/api/order/confirm/${orderId}`);
+        localStorage.removeItem("orderId");
+        setLoader(false);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (message === 'succeeded') {
+      update_payment();
+    }
+  }, [message]);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-4">
+      {message === "failed" || message === "processing" ? (
+        <>
+          <img src="/images/error.png" alt="" />
+          <Link
+            className="px-5 py-2 text-white bg-green-500 rounded-sm"
+            to="/dashboard/my-orders"
+          >
+            Back To Dashboard
+          </Link>
+        </>
+      ) : message === "succeeded" ? (
+        loader ? (
+          <FadeLoader />
+        ) : (
+          <>
+            <img src="/images/success.png" alt="" />
+            <Link
+              className="px-5 py-2 text-white bg-green-500 rounded-sm"
+              to="/dashboard/my-orders"
+            >
+              Back To Dashboard
+            </Link>
+          </>
+        )
+      ) : (
+        <FadeLoader />
+      )}
+    </div>
+  );
+};
+
+export default ConfirmOrder;
