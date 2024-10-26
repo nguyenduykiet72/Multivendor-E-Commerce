bc832b89ec396ddacdc6d2460bfb31eb4d2a5b33
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-10-26 20:37:39 +0700

    hoan thanh show banner

--- backend-Ecommerce/src/controllers/dashboard/dashboardController.js
+++ backend-Ecommerce/src/controllers/dashboard/dashboardController.js
@@ -7,8 +7,10 @@ const sellerWalletModel = require("../../models/sellerWalletModel");
 const authOrderModel = require("../../models/authOrderModel");
 const sellerCustomerMessageModel = require("../../models/chat/sellerCustomerMessageModel");
 const { default: mongoose } = require("mongoose");
-
+const { formidable } = require("formidable");
+const cloudinary = require("cloudinary").v2;
 const { responseReturn } = require("../../utils/response");
+const bannerModel = require("../../models/bannerModel");
 
 const get_admin_dashboard_data = async (req, res) => {
   const { id } = req;
@@ -115,7 +117,113 @@ const get_seller_dashboard_data = async (req, res) => {
   }
 };
 
+const add_banner = async (req, res) => {
+  const form = formidable({ multiples: true });
+  form.parse(req, async (error, field, files) => {
+    const { productId } = field;
+    const { mainBanner } = files;
+
+    cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET_KEY,
+      secure: true,
+    });
+
+    try {
+      const { slug } = await productModel.findById(productId);
+      const result = await cloudinary.uploader.upload(mainBanner.filepath, {
+        folder: "banners",
+      });
+      const banner = await bannerModel.create({
+        productId,
+        banner: result.url,
+        link: slug,
+      });
+      responseReturn(res, 201, {
+        banner,
+        message: "Banner added successfully",
+      });
+    } catch (error) {
+      console.log(error.message);
+      responseReturn(res, 500, { error: "Internal server error" });
+    }
+  });
+};
+
+const get_banner = async (req, res) => {
+  const { productId } = req.params;
+  try {
+    const banner = await bannerModel.findOne({
+      productId: mongoose.Types.ObjectId.createFromHexString(productId),
+    });
+    responseReturn(res, 200, { banner });
+  } catch (error) {
+    console.log(error.message);
+    responseReturn(res, 500, { error: "Internal server error" });
+  }
+};
+
+const update_banner = async (req, res) => {
+  const { bannerId } = req.params;
+  const form = formidable({ multiples: true });
+  form.parse(req, async (error, _, files) => {
+    const { mainBanner } = files;
+
+    cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET_KEY,
+      secure: true,
+    });
+
+    try {
+      let banner = await bannerModel.findById(bannerId);
+      let temp = banner.banner.split("/");
+      temp = temp[temp.length - 1];
+      const imageName = temp.split(".")[0];
+      await cloudinary.uploader.destroy(imageName);
+      const { url } = await cloudinary.uploader.upload(mainBanner.filepath, {
+        folder: "banners",
+      });
+
+      await bannerModel.findByIdAndUpdate(bannerId, {
+        banner: url,
+      });
+
+      banner = await bannerModel.findById(bannerId);
+      responseReturn(res, 200, {
+        banner,
+        message: "Banner updated successfully",
+      });
+    } catch (error) {
+      console.log(error.message);
+      responseReturn(res, 500, { error: "Internal server error" });
+    }
+  });
+};
+
+const get_banners = async (req, res) => {
+  try {
+    const banners = await bannerModel.aggregate([
+      {
+        $sample: {
+          size: 5,
+        }
+      }
+    ]);
+    responseReturn(res, 200, { banners });
+  } catch (error) {
+    console.log(error.message);
+    responseReturn(res, 500, { error: "Internal server error" });
+  }
+};
+
 module.exports = {
   get_admin_dashboard_data,
   get_seller_dashboard_data,
+  add_banner,
+  get_banner,
+  update_banner,
+  get_banners,
 };
--- backend-Ecommerce/src/routes/dashboard/dashboardRoutes.js
+++ backend-Ecommerce/src/routes/dashboard/dashboardRoutes.js
@@ -6,6 +6,10 @@ const router = require("express").Router();
 
 router.get("/admin/get-dashboard-data", authMiddleware, dashboardController.get_admin_dashboard_data);
 router.get("/seller/get-dashboard-data", authMiddleware, dashboardController.get_seller_dashboard_data);
+router.post("/banner/add", authMiddleware, dashboardController.add_banner);
+router.get("/banner/get/:productId", authMiddleware, dashboardController.get_banner);
+router.put("/banner/update/:bannerId", authMiddleware, dashboardController.update_banner);
+router.get("/banners", dashboardController.get_banners);
 
 
 module.exports = router;
