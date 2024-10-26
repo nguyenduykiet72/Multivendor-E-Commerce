a6655e19c8fa267e6f0a1a1277468241e5a0f4ad
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-10-26 23:00:01 +0700

    them router va logic bussiness

--- backend-Ecommerce/src/controllers/dashboard/categoryControllers.js
+++ backend-Ecommerce/src/controllers/dashboard/categoryControllers.js
@@ -85,7 +85,72 @@ const get_category = async (req, res) => {
   }
 };
 
+const update_category = async (req, res) => {
+  const form = formidable();
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      responseReturn(res, 404, { error: "something went wrong" });
+    } else {
+      let { name } = fields;
+      let { image } = files;
+      const { id } = req.params;
+
+      name = name.trim();
+      const slug = name.split(" ").join("-");
+
+      try {
+        let result = null;
+        if (image) {
+          cloudinary.config({
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.API_KEY,
+            api_secret: process.env.API_SECRET_KEY,
+            secure: true,
+          });
+          result = await cloudinary.uploader.upload(image.filepath, {
+            folder: "categories",
+          });
+        }
+
+        const updateData = {
+          name,
+          slug,
+        };
+        if (result) {
+          updateData.image = result.url;
+        }
+        const category = await categoryModel.findByIdAndUpdate(id, updateData, {
+          new: true,
+        });
+        responseReturn(res, 200, {
+          category,
+          message: "Category Updated Successfully",
+        });
+      } catch (error) {
+        responseReturn(res, 500, { error: "Internal Server Error" });
+      }
+    }
+  });
+};
+
+const delete_category = async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const deleteCategory = await categoryModel.findByIdAndDelete(categoryId);
+    if (!deleteCategory) {
+      console.log(`Category with id ${categoryId} not found`);
+      return res.status(404).json({ message: "Category not found" });
+    }
+    res.status(200).json({ message: "Category deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 module.exports = {
   add_category,
   get_category,
-}
+  update_category,
+  delete_category,
+};
