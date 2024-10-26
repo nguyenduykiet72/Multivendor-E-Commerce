59504530fa604a689cd3fd19f78d63a222c1866a
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-10-26 20:39:10 +0700

    hoan thanh banner

---
+++ frontend-Ecommerce/dashboard/src/components/seller/AddBanner.jsx
@@ -0,0 +1,161 @@
+import React, { useEffect, useState } from "react";
+import { BsFillImageFill } from "react-icons/bs";
+import { PropagateLoader } from "react-spinners";
+import { overrideStyle } from "../../utils/util";
+import { useDispatch, useSelector } from "react-redux";
+import { add_banner, get_banner, update_banner } from "../../store/Reducers/bannerReducer";
+import { useParams } from "react-router-dom";
+import toast from "react-hot-toast";
+import { messageClear } from "../../store/Reducers/productReducer";
+import { FaRegImage } from "react-icons/fa";
+
+const AddBanner = () => {
+  const { productId } = useParams();
+  const dispatch = useDispatch();
+
+  const { loader, successMessage, errorMessage, banner } = useSelector(
+    (state) => state.banner
+  );
+
+  const [imageShow, setImageShow] = useState("");
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    if (successMessage) {
+      toast.success(successMessage);
+      dispatch(messageClear());
+    }
+    if (errorMessage) {
+      toast.error(errorMessage);
+      dispatch(messageClear());
+    }
+  }, [successMessage, errorMessage]);
+
+  const imageHandle = (e) => {
+    const files = e.target.files;
+    const length = files.length;
+
+    if (length > 0) {
+      setImage(files[0]);
+      setImageShow(URL.createObjectURL(files[0]));
+    }
+  };
+
+  const add = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("productId", productId);
+    formData.append("mainBanner", image);
+    dispatch(add_banner(formData));
+  };
+
+  const update = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("mainBanner", image);
+    dispatch(update_banner({ info: formData, bannerId: banner._id }));
+  };
+
+  useEffect(() => {
+    dispatch(get_banner(productId));
+  }, [productId]);
+
+  return (
+    <div className="px-2 pt-5 lg:px-7">
+      <h1 className="text-[#000000] font-semibold text-lg mb-3">Add Banner</h1>
+      <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
+        {!banner && (
+          <div>
+            <form onSubmit={add}>
+              <div className="mb-4">
+                <label
+                  className="flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed hover:border-red-500 w-full text-white"
+                  htmlFor="image"
+                >
+                  <span className="text-4xl">
+                    <FaRegImage />
+                  </span>
+                  <span>Select Banner Image </span>
+                </label>
+                <input
+                  required
+                  onChange={imageHandle}
+                  className="hidden"
+                  type="file"
+                  id="image"
+                />
+              </div>
+
+              {imageShow && (
+                <div className="mb-4">
+                  <img className="w-full h-[300px]" src={imageShow} alt="" />
+                </div>
+              )}
+
+              <button
+                disabled={loader ? true : false}
+                className="bg-red-500 w-[280px] hover:shadow-red-300/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3"
+              >
+                {loader ? (
+                  <PropagateLoader color="#fff" cssOverride={overrideStyle} />
+                ) : (
+                  "Add Banner"
+                )}
+              </button>
+            </form>
+          </div>
+        )}
+
+        {banner && (
+          <div>
+            {
+              <div className="mb-4">
+                <img className="w-full h-[300px]" src={banner.banner} alt="" />
+              </div>
+            }
+
+            <form onSubmit={update}>
+              <div className="mb-4">
+                <label
+                  className="flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed hover:border-red-500 w-full text-white"
+                  htmlFor="image"
+                >
+                  <span className="text-4xl">
+                    <FaRegImage />
+                  </span>
+                  <span>Select Banner Image </span>
+                </label>
+                <input
+                  required
+                  onChange={imageHandle}
+                  className="hidden"
+                  type="file"
+                  id="image"
+                />
+              </div>
+
+              {imageShow && (
+                <div className="mb-4">
+                  <img className="w-full h-[300px]" src={imageShow} alt="" />
+                </div>
+              )}
+
+              <button
+                disabled={loader ? true : false}
+                className="bg-red-500 w-[280px] hover:shadow-red-300/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3"
+              >
+                {loader ? (
+                  <PropagateLoader color="#fff" cssOverride={overrideStyle} />
+                ) : (
+                  "Update Banner"
+                )}
+              </button>
+            </form>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AddBanner;
---
+++ frontend-Ecommerce/dashboard/src/store/Reducers/bannerReducer.js
@@ -0,0 +1,97 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+export const add_banner = createAsyncThunk(
+  "banner/add_banner",
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.post("/banner/add", info, {
+        withCredentials: true,
+      });
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const get_banner = createAsyncThunk(
+  "banner/get_banner",
+  async (productId, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.get(`/banner/get/${productId}`, {
+        withCredentials: true,
+      });
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const update_banner = createAsyncThunk(
+  "banner/update_banner",
+  async ({bannerId, info}, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.put(`/banner/update/${bannerId}`, info,{
+        withCredentials: true,
+      });
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const bannerReducer = createSlice({
+  name: "banner",
+  initialState: {
+    successMessage: "",
+    errorMessage: "",
+    loader: false,
+    banners: [],
+    banner: "",
+  },
+  reducers: {
+    messageClear: (state, _) => {
+      state.errorMessage = "";
+      state.successMessage = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+    .addCase(add_banner.pending, (state, { payload }) => {
+      state.loader = true;
+    })
+    .addCase(add_banner.rejected, (state, { payload }) => {
+      state.loader = false;
+      state.errorMessage = payload.error;
+    })
+    .addCase(add_banner.fulfilled, (state, { payload }) => {
+      state.loader = false;
+      state.successMessage = payload.message;
+      state.banner = payload.banner;
+    })
+
+    .addCase(get_banner.fulfilled, (state, { payload }) => {
+      state.banner = payload.banner;
+    })
+
+    .addCase(update_banner.pending, (state, { payload }) => {
+        state.loader = true;
+      })
+      .addCase(update_banner.rejected, (state, { payload }) => {
+        state.loader = false;
+        state.errorMessage = payload.error;
+      })
+      .addCase(update_banner.fulfilled, (state, { payload }) => {
+        state.loader = false;
+        state.successMessage = payload.message;
+        state.banner = payload.banner;
+      })
+  
+  },
+});
+
+export const { messageClear } = bannerReducer.actions;
+export default bannerReducer.reducer;
